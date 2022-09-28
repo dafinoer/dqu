a974dc4466cd4cf6df56b22db0e44e8e6aba4f87
dafinoer <dafinoer@outlook.com>
2022-09-28 17:10:36 +0700

    revamp home

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -5,10 +5,33 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Welcome',
-        style: TextStyle(color: Colors.black),
+    final theme = Theme.of(context);
+    return SizedBox.expand(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.sunny, color: Colors.yellow[700], size: 50.0),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: Icon(Icons.dark_mode_rounded, size: 50.0),
+              )
+            ],
+          ),
+          Text('DZIKIR', style: theme.textTheme.headline1),
+          RichText(
+              text: TextSpan(
+            children: [
+              TextSpan(text: 'Pagi', style: theme.textTheme.headline3),
+              TextSpan(text: ' & ', style: theme.textTheme.headline3),
+              TextSpan(text: 'Petang', style: theme.textTheme.headline3)
+            ],
+          ))
+        ],
       ),
     );
   }
