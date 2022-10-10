459fde8c5073de4fdc9eefef4f8576d13f1797b4
dafinoer <dafinoer@outlook.com>
2022-10-10 09:52:07 +0700

    imrove speed

--- lib/controller/dzikir_list/dzikir_list_controller.dart
+++ lib/controller/dzikir_list/dzikir_list_controller.dart
@@ -6,18 +6,31 @@ class DzikirListController extends StateNotifier<List<Dzikir>> {
   DzikirListController(super.state, this.dzikirRepository);
 
   final DzikirRepository dzikirRepository;
+  final Map<String, List<Dzikir>> dzikirGroupByType = {};
 
   void onDzikirDay() {
-    final dzikrGroups = dzikirRepository.onGetDzikirList();
-    final dzikirDayList =
-        dzikrGroups.where((element) => element.isDay ?? false).toList();
-    state = dzikirDayList;
+    if (dzikirGroupByType.containsKey('days')) {
+      final dzikirGroups = dzikirGroupByType['days'] ?? List.empty();
+      state = dzikirGroups;
+    } else {
+      final dzikrGroups = dzikirRepository.onGetDzikirList();
+      final dzikirDayList =
+          dzikrGroups.where((element) => element.isDay ?? false).toList();
+      dzikirGroupByType.putIfAbsent('days', () => dzikirDayList);
+      state = dzikirDayList;
+    }
   }
 
   void onAfternoonDzikir() {
-    final dzikirGroups = dzikirRepository.onGetDzikirList();
-    final afternoonDzikir =
-        dzikirGroups.where((element) => element.isNight ?? false).toList();
-    state = afternoonDzikir;
+    if (dzikirGroupByType.containsKey('night')) {
+      final dzikirGroups = dzikirGroupByType['night'] ?? List.empty();
+      state = dzikirGroups;
+    } else {
+      final dzikirGroups = dzikirRepository.onGetDzikirList();
+      final afternoonDzikir =
+          dzikirGroups.where((element) => element.isNight ?? false).toList();
+      dzikirGroupByType.putIfAbsent('night', () => afternoonDzikir);
+      state = afternoonDzikir;
+    }
   }
 }
